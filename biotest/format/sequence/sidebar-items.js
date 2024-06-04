385f0e6450b8ea8077ff07bdf05c52cc5efd9951
natir <natir@users.noreply.github.com>
2024-06-04 13:53:40 +0000

    deploy: 2811a737a93695565b82ae277ee1caa8f760b9d6

---
+++ biotest/format/sequence/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["SequenceBuilderError"],"struct":["Sequence","SequenceBuilder"]};
--- biotest/format/sidebar-items.js
+++ biotest/format/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["fasta","fastq","vcf"],"trait":["Format"]};
+window.SIDEBAR_ITEMS = {"mod":["fasta","fastq","sequence","vcf"],"trait":["Format"]};
